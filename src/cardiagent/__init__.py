ba6cb7e6152c244c281ae8b510d8ffca5c0136a0
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-08-18 00:58:25 +0300

    Expose literature archetypes

--- src/cardiagent/__init__.py
+++ src/cardiagent/__init__.py
@@ -4,33 +4,18 @@ from .adaptive import AdaptiveChallengeEngine, AdaptiveScore, CurriculumStage, D
 from .benchmark import BlindBenchmark, BlindCase, build_blind_benchmark
 from .generator import ChallengeGenerator
 from .handoff import BlindCardiVexHandoff, CardiVexHandoff, create_blind_handoff, create_handoff
+from .literature import LITERATURE_ARCHETYPES, LiteratureArchetype, archetypes_by_domain, get_archetype
 from .manifest import ChallengeManifest, build_manifest
 from .ml import AgentGeneratorModel, generate_ml_agents, generate_ml_benchmark, train_agent_model
 from .models import ChallengeAgent, ChallengeDomain, PhenotypeProfile
 from .quality import PopulationReport, assess_population
 
 __all__ = [
-    "ChallengeAgent",
-    "ChallengeDomain",
-    "PhenotypeProfile",
-    "ChallengeGenerator",
-    "CardiVexHandoff",
-    "BlindCardiVexHandoff",
-    "create_handoff",
-    "create_blind_handoff",
-    "ChallengeManifest",
-    "build_manifest",
-    "BlindCase",
-    "BlindBenchmark",
-    "build_blind_benchmark",
-    "AgentGeneratorModel",
-    "train_agent_model",
-    "generate_ml_agents",
-    "generate_ml_benchmark",
-    "DetectionOutcome",
-    "AdaptiveScore",
-    "CurriculumStage",
-    "AdaptiveChallengeEngine",
-    "PopulationReport",
-    "assess_population",
+    "ChallengeAgent", "ChallengeDomain", "PhenotypeProfile", "ChallengeGenerator",
+    "CardiVexHandoff", "BlindCardiVexHandoff", "create_handoff", "create_blind_handoff",
+    "ChallengeManifest", "build_manifest", "BlindCase", "BlindBenchmark", "build_blind_benchmark",
+    "AgentGeneratorModel", "train_agent_model", "generate_ml_agents", "generate_ml_benchmark",
+    "DetectionOutcome", "AdaptiveScore", "CurriculumStage", "AdaptiveChallengeEngine",
+    "PopulationReport", "assess_population",
+    "LiteratureArchetype", "LITERATURE_ARCHETYPES", "archetypes_by_domain", "get_archetype",
 ]
